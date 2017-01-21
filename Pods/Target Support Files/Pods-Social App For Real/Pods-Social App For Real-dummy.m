9cf815e894b0809c66f891e47b12b2819a208fdd
BrettForeman <bfcollege7@yahoo.com>
2017-01-20 19:10:29 -0800

    first commit

---
+++ Pods/Target Support Files/Pods-Social App For Real/Pods-Social App For Real-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Social_App_For_Real : NSObject
+@end
+@implementation PodsDummy_Pods_Social_App_For_Real
+@end
---
+++ Pods/Target Support Files/Pods-Social App For Real/Pods-Social App For Real-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Social_App_For_RealVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Social_App_For_RealVersionString[];
+
